845cc0f52035868919149748ff2933e61de61d93
Aquiles Carattino <aquiles@aquicarattino.com>
2017-07-18 17:42:16 +0200

    Started to work in general models

--- pharos/model/lib/general_functions.py
+++ pharos/model/lib/general_functions.py
@@ -7,29 +7,36 @@ from pharos.model.lib.device import device
 def from_yaml_to_devices(filename = "config/devices.yml", mod=None):
     """ Reads a YAML file and returns a list of devices.
     :param filename: File where the data is stored
-    :param mod: The type of device to look for
+    :param mod: The type of device to look for, for example: {'connection': {'device': 'NI-DAQ'}}
     :return: list of :class: device
     """
 
     stream = open(filename, 'r')
-    devices = yaml.load(stream)['devices']
-    print(devices)
+    devices = yaml.load(stream)
+
     stream.close()
     devs = []
     for d in devices:
-        if mod != None:
-            if devices[d]['type'] == mod:
-                for dev in devices[d]['devices']:
-                    dd = device(devices[d]['devices'][dev])
+        dev = devices[d]
+        if mod is not None:
+            k = list(mod.keys())[0] # Extract the key from the condition
+            print(k)
+            v = mod[k]
+            print(v)
+            try:
+                if dev[k] == v:
+                    dd = device(dev)
                     devs.append(dd)
                     print(dd)
+            except:
+                print('except')
+                pass
         else:
-            for dev in devices[d]['devices']:
-                dd = device(devices[d]['devices'][dev])
-                devs.append(dd)
-                print(dd)
+            dd = device(dev)
+            devs.append(dd)
+            print(dd)
     return devs
 
 if __name__ == "__main__":
-    d = from_yaml_to_devices('../../config/devices.yml', mod='NI-DAQ')
-    print(d)
+    d = from_yaml_to_devices('../../config/devices.yml',mod={'connection': {'type': 'daq'}})
+   # print(d)

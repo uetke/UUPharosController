af57ea811e75e6cea5e282eaabfef0a72cb6e799
Aquiles Carattino <aquiles@aquicarattino.com>
2017-07-25 16:31:52 +0200

    new variables to config

--- pharos/config/config.py
+++ pharos/config/config.py
@@ -1,6 +1,7 @@
 import PyDAQmx as nidaq
 
-ni_buffer = 10000 # When acquiring in continuous mode, how big is the buffer.
+ni_buffer = 10000  # When acquiring in continuous mode, how big is the buffer.
 ni_measure_mode = nidaq.DAQmx_Val_Diff
 ni_trigger_edge = nidaq.DAQmx_Val_Rising
 ni_read_timeout = 0.1
+monitor_read_scan = 10  # How many times do we update the signal during 1 wavelength sweep

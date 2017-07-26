a63f7609aa6e0ffaa4d269f4f68fb51667fd0c81
Aquiles Carattino <aquiles@aquicarattino.com>
2017-07-26 11:20:03 +0200

    Progress on scan GUI. Updated reqs

--- pharos/view/GUI/scan_config_widget.py
+++ pharos/view/GUI/scan_config_widget.py
@@ -1,6 +1,7 @@
 import os
 from PyQt4 import QtCore, QtGui, uic
-
+from lantz import Q_
+from PyQt4.QtCore import pyqtSlot
 
 class ScanConfigWidget(QtGui.QWidget):
     def __init__(self, parent=None):
@@ -12,6 +13,8 @@ class ScanConfigWidget(QtGui.QWidget):
         self.devices = {}
 
     def populate_daq_devices(self, daqs):
+        """ Adds group boxes for all the output devices connected to the daqs.
+        :param daqs: Dictionary of daqs containing the devices connected Check the measurement class for the definition."""
         for d in daqs:
             self.groupbox.append(QtGui.QGroupBox())
             self.groupbox[-1].setTitle(d)
@@ -22,56 +25,80 @@ class ScanConfigWidget(QtGui.QWidget):
                 device_description = dev.properties['description']
 
                 tick = QtGui.QCheckBox()
-                min_line = QtGui.QLineEdit()
-                max_line = QtGui.QLineEdit()
-                step_line = QtGui.QLineEdit()
-
-                inputs = {
+                max = QtGui.QLineEdit(dev.properties['limits']['max'])
+                min = QtGui.QLineEdit(dev.properties['limits']['min'])
+                step = QtGui.QLineEdit()
+                # min_line.editingFinished.connect(lambda: self.check_limits(device_name, min_line.text()))
+                self.devices[device_name] = {
                     'tick': tick,
-                    'max': max_line,
-                    'min': min_line,
-                    'step': step_line,
+                    'max_line': max,
+                    'min_line': min,
+                    'step_line': step,
+                    'max': dev.properties['limits']['max'],
+                    'min': dev.properties['limits']['min'],
                 }
 
-                self.devices.update(inputs)
+                min.editingFinished.connect(self.check_limits)
+                max.editingFinished.connect(self.check_limits)
+                step.editingFinished.connect(self.check_limits)
 
                 label = QtGui.QLabel(device_name)
                 label.setToolTip(device_description)
 
-                layout.addWidget(self.ticks[-1], i, 0)
+                layout.addWidget(tick, i, 0)
                 layout.addWidget(label, i, 1)
-                layout.addWidget(min_line, i, 2)
-                layout.addWidget(max_line, i, 3)
-                layout.addWidget(step_line, i, 4)
+                layout.addWidget(min, i, 2)
+                layout.addWidget(max, i, 3)
+                layout.addWidget(step, i, 4)
 
-                QtCore.QObject.connect(min_line, QtCore.SIGNAL('editingFinished()'), lambda: self.check_limits(device_name, min_line.text()))
-
-                i+=1
+                i += 1
 
             self.groupbox[-1].setLayout(layout)
             self.layout_widgets.addRow(self.groupbox[-1])
 
-    def check_limits(self, dev, text):
-        print(dev)
-        print(text)
+    def populate_other_devices(self, devs):
+        """ Adds group boxes for every device and includes the parameters that can be tuned in every case. """
+
+    def check_limits(self):
+        red_background =  "QLineEdit { background: rgb(255, 20, 20); selection-background-color: rgb(233, 99, 0); }"
+        white_background = "QLineEdit { background: rgb(255, 255, 255); selection-background-color: rgb(233, 99, 0); }"
+        for dev in self.devices:
+            dev = self.devices[dev]
+            if Q_(dev['min']) <= Q_(dev['min_line'].text()) <= Q_(dev['max']):
+                dev['min_line'].setStyleSheet(white_background)
+            else:
+                dev['min_line'].setStyleSheet(red_background)
+            if Q_(dev['min']) <= Q_(dev['max_line'].text()) <= Q_(dev['max']):
+                dev['max_line'].setStyleSheet(white_background)
+            else:
+                dev['max_line'].setStyleSheet(red_background)
+
+
 
 if __name__ == "__main__":
+    import sys
+
     class dev:
         pass
     dev1 = dev()
     dev1.properties = {'name': "Name 1",
-            "description": "Description 1"}
+            "description": "Description 1",
+                       'limits': {'min': '10nm',
+                                  'max': '20nm'}
+                       }
     dev2 = dev()
     dev2.properties = {'name': 'Name 2',
-                       'description': 'Description 2'}
-
-    daqs = {}
-
-    daqs['DAQ 1'] = {
+                       'description': 'Description 2',
+                       'limits': {'min': '10um',
+                       'max': '20um'}
+                       }
+    daqs = {'DAQ 1': {
         'output': [dev1, dev2],
-    }
+    },
+    'DAQ 2': {
+        'output': [dev2],
+    }}
 
-    import sys
 
     app = QtGui.QApplication(sys.argv)
     window = ScanConfigWidget()

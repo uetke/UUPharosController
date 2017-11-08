7775a1e099c8a17c3ba41337ef99f84d627e13af
Aquiles Carattino <aquiles@aquicarattino.com>
2017-11-08 16:07:34 +0100

    Lorentz Fit show/hide

--- pharos/view/GUI/monitor_with_memory.py
+++ pharos/view/GUI/monitor_with_memory.py
@@ -45,6 +45,12 @@ class MonitorMemory(QtGui.QMainWindow):
         self.p = None
         self.p1 = None
         self.p2 = None
+        self.lorentz_plot1 = None
+        self.lorentz_plot2 = None
+        self.lorentz_plot = None
+        self.params = None
+        self.params1 = None
+        self.params2 = None
         self.name = None
         self.id = None
         self.show_label = False
@@ -58,14 +64,18 @@ class MonitorMemory(QtGui.QMainWindow):
         self.fit_lorentzian_action = QtGui.QAction("Fit a Lorentzian", self.main_plot.plotItem.vb.menu)
         self.fit_lorentzian_action.triggered.connect(self.fit_lorentzian)
         self.main_plot.plotItem.vb.menu.addAction(self.fit_lorentzian_action)
+        self.hide_lorentzian_action = QtGui.QAction("Hide Lorentzian", self.main_plot.plotItem.vb.menu)
+        self.hide_lorentzian_action.triggered.connect(self.hide_lorentzian)
+        self.main_plot.plotItem.vb.menu.addAction(self.hide_lorentzian_action)
+
         self.directory = None
 
     def clear_data(self):
-        self.layout.removeWidget(self.main_plot)
+        self.central_widget.layout().removeWidget(self.main_plot)
         self.main_plot.deleteLater()
         self.main_plot = pg.PlotWidget()
         self.main_plot.setLabel('bottom', 'Wavelength (nm)', **{'color': '#FFF', 'font-size': '14pt'})
-        self.layout.addWidget(self.main_plot)
+        self.central_widget.layout().addWidget(self.main_plot)
         self.ydata = None
         self.wavelength = None
         self.wavelength2 = None
@@ -74,6 +84,15 @@ class MonitorMemory(QtGui.QMainWindow):
         self.p1 = None
         self.p2 = None
         self.p = None
+        self.show_label = False
+        self.do_average_plot = False
+        self.lorentz_fitting = False
+        self.lorentz_plot1 = None
+        self.lorentz_plot2 = None
+        self.lorentz_plot = None
+        self.params = None
+        self.params1 = None
+        self.params2 = None
 
     def set_wavelength(self, wavelength):
         self.wavelength = wavelength
@@ -91,6 +110,10 @@ class MonitorMemory(QtGui.QMainWindow):
                 self.p1[-1].setDownsampling(auto=True, method='peak')
                 self.p2.append(self.main_plot.plot(self.wavelength, d2))
                 self.p2[-1].setDownsampling(auto=True, method='peak')
+            self.lorentz_plot1 = self.main_plot.plot(self.wavelength, d1)
+            self.lorentz_plot2 = self.main_plot.plot(self.wavelength2, d2)
+            self.lorentz_plot2.hide()
+            self.lorentz_plot1.hide()
         else:
             self.ydata = np.zeros((self.memory, len(self.wavelength)))
             self.len_ydata = int(len(self.wavelength))
@@ -98,9 +121,12 @@ class MonitorMemory(QtGui.QMainWindow):
             for i in range(self.memory):
                 self.p.append(self.main_plot.plot(self.wavelength, self.ydata[-1, :]))
                 self.p[-1].setDownsampling(auto=True, method='peak')
+            self.lorentz_plot = self.main_plot.plot(self.wavelength, self.ydata[-1, :])
+            self.lorentz_plot.setPen({'color': '#b6db00', 'width': 2})
+            self.lorentz_plot.hide()
+
         self.set_pens()
         self.main_plot.scene().sigMouseMoved.connect(self.print_mouse)
-
 
     def set_ydata(self, values):
         val_len = len(values)
@@ -127,7 +153,7 @@ class MonitorMemory(QtGui.QMainWindow):
             if self.do_average_plot:
                 data_mean = np.mean(self.ydata, axis=0)
                 self.p1[0].setData(self.wavelength, data_mean[:int(self.len_ydata/2)])
-                self.p2[0].setData(self.wavelength, data_mean[int(self.len_ydata/2):])
+                self.p2[0].setData(self.wavelength2, data_mean[int(self.len_ydata/2):])
         else:
             for i in range(self.memory):
                 d = self.ydata[-i-1, :]
@@ -225,6 +251,12 @@ class MonitorMemory(QtGui.QMainWindow):
                     self.p[i].setPen({'color': '#b6dbff', 'width': 1/(i+1)})
                 self.p[-1].setPen({'color': '#b6dbff', 'width': 3})
 
+        if self.two_way:
+            self.lorentz_plot1.setPen({'color': '#b6aa00', 'width': 2.5})
+            self.lorentz_plot2.setPen({'color': '#ffaa00', 'width': 2.5})
+        else:
+            self.lorentz_plot.setPen({'color': '#b6aa00', 'width': 2.5})
+
     def make_average(self):
         if not self.do_average_plot:
             self.do_average_plot = True
@@ -236,57 +268,103 @@ class MonitorMemory(QtGui.QMainWindow):
             self.update_monitor()
 
     def fit_lorentzian(self):
+        self.lorentz_fitting = True
+        if self.do_average_plot:
+            data = np.mean(self.ydata, axis=0)
+        else:
+            data = self.ydata[-1, :]
 
-        if not self.lorentz_fitting:
-            self.lorentz_fitting = True
-            if self.do_average_plot:
-                data = np.mean(self.ydata, axis=0)
-            else:
-                data = self.ydata[-1, :]
+        if self.two_way:
+            d1 = data[:int(self.len_ydata/2)]
+            d2 = data[int(self.len_ydata/2):]
+            p0 = [self.y_mouse-np.min(d1), self.x_mouse, 1, np.min(d1)]
+            solp1, ier = leastsq(errorfunc,
+                                 p0,
+                                 args=(self.wavelength, d1),
+                                 Dfun=None,
+                                 full_output=False,
+                                 ftol=1e-9,
+                                 xtol=1e-9,
+                                 maxfev=100000,
+                                 epsfcn=1e-10,
+                                 factor=0.1)
+            self.lorentz_plot1.setData(self.wavelength, lorentz(solp1, self.wavelength))
+            self.lorentz_plot1.show()
 
-            if self.two_way:
-                d1 = data[:int(self.len_ydata/2)]
-                d2 = data[int(self.len_ydata/2):]
-                p0 = [self.y_mouse-np.min(d1), self.x_mouse, 1, np.min(d1)]
-                solp1, ier = leastsq(errorfunc,
-                                     p0,
-                                     args=(self.wavelength, d1),
-                                     Dfun=None,
-                                     full_output=False,
-                                     ftol=1e-9,
-                                     xtol=1e-9,
-                                     maxfev=100000,
-                                     epsfcn=1e-10,
-                                     factor=0.1)
-                self.lorentz_plot1 = self.main_plot.plot(self.wavelength, lorentz(solp1, self.wavelength))
+            p0 = [self.y_mouse - np.min(d2), self.x_mouse, 1, np.min(d2)]
+            solp2, ier = leastsq(errorfunc,
+                                 p0,
+                                 args=(self.wavelength2, d2),
+                                 Dfun=None,
+                                 full_output=False,
+                                 ftol=1e-9,
+                                 xtol=1e-9,
+                                 maxfev=100000,
+                                 epsfcn=1e-10,
+                                 factor=0.1)
 
-                p0 = [self.y_mouse - np.min(d2), self.x_mouse, 1, np.min(d2)]
-                solp2, ier = leastsq(errorfunc,
-                                     p0,
-                                     args=(self.wavelength2, d2),
-                                     Dfun=None,
-                                     full_output=False,
-                                     ftol=1e-9,
-                                     xtol=1e-9,
-                                     maxfev=100000,
-                                     epsfcn=1e-10,
-                                     factor=0.1)
+            self.lorentz_plot2.setData(self.wavelength2, lorentz(solp2, self.wavelength2))
+            self.lorentz_plot2.show()
 
-                self.lorentz_plot2 = self.main_plot.plot(self.wavelength, lorentz(solp2, self.wavelength))
+            ampl = solp1[0]-solp1[3]
+            center = solp1[1]
+            width = np.sqrt(solp1[2])*2
+            offset = solp1[3]
+            if self.params1 is None:
+                self.params1 = QtGui.QLineEdit()
+                self.central_widget.layout().addWidget(self.params1)
 
-            else:
-                p0 = [self.y_mouse - np.min(data), self.x_mouse, 1, np.min(data)]
-                solp, ier = leastsq(errorfunc,
-                                     p0,
-                                     args=(self.wavelength, data),
-                                     Dfun=None,
-                                     full_output=False,
-                                     ftol=1e-9,
-                                     xtol=1e-9,
-                                     maxfev=100000,
-                                     epsfcn=1e-10,
-                                     factor=0.1)
-                self.lorentz_plot = self.main_plot.plot(self.wavelength, lorentz(solp, self.wavelength))
+            self.params1.setText("Amplitude: {}, Center: {}, Width: {}, Offset: {}".format(ampl, center, width, offset))
+            self.params1.show()
+
+            ampl = solp2[0] - solp2[3]
+            center = solp2[1]
+            width = np.sqrt(solp2[2]) * 2
+            offset = solp2[3]
+            if self.params2 is None:
+                self.params2 = QtGui.QLineEdit()
+                self.central_widget.layout().addWidget(self.params2)
+            self.params2.setText("Amplitude: {}, Center: {}, Width: {}, Offset: {}".format(ampl, center, width, offset))
+            self.params2.show()
+
+        else:
+            p0 = [self.y_mouse - np.min(data), self.x_mouse, 1, np.min(data)]
+            solp, ier = leastsq(errorfunc,
+                                 p0,
+                                 args=(self.wavelength, data),
+                                 Dfun=None,
+                                 full_output=False,
+                                 ftol=1e-9,
+                                 xtol=1e-9,
+                                 maxfev=100000,
+                                 epsfcn=1e-10,
+                                 factor=0.1)
+            self.lorentz_plot.setData(self.wavelength, lorentz(solp, self.wavelength))
+            self.lorentz_plot.show()
+            ampl = solp[0] - solp[3]
+            center = solp[1]
+            width = np.sqrt(solp[2]) * 2
+            offset = solp[3]
+            if self.params is None:
+                self.params = QtGui.QLineEdit()
+                self.central_widget.layout().addWidget(self.params)
+
+            self.params.setText(
+                "Amplitude: {}, Center: {}, Width: {}, Offset: {}".format(ampl, center, width, offset))
+            self.params.show()
+
+
+    def hide_lorentzian(self):
+        self.lorentz_fitting = False
+        if self.two_way:
+            self.lorentz_plot2.hide()
+            self.lorentz_plot1.hide()
+            self.params2.hide()
+            self.params1.hide()
+        else:
+            self.lorentz_plot.hide()
+            self.params.hide()
+
 
 if __name__ == '__main__':
     import sys
@@ -295,17 +373,15 @@ if __name__ == '__main__':
     wavelength = np.linspace(1492, 1512, 500)
     ap = QApplication(sys.argv)
     m = MonitorMemory()
-    m.memory = 1
+    m.memory = 2
     m.two_way = True
     m.set_wavelength(wavelength)
     m.set_name('Test')
-    d1 = lorentz([1, 1505, 3, 4], wavelength)
+    d1 = lorentz([10, 1505, 3, 4], wavelength)
     d2 = lorentz([5, 1500, 2.5, 3.4], wavelength)
-    m.set_ydata(d1)
-    m.set_ydata(d2)
-    # for _ in range(5):
-    #     data = np.random.random(500)
-    #     m.set_ydata(data)
-    #
+    for _ in range(12):
+        data = np.random.random(500)*.2
+        m.set_ydata(data+d1)
+
     m.show()
     ap.exit(ap.exec_())

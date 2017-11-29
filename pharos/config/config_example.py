56efda93e7b615e9e2b2ad5bb000125fc81357e0
Aquiles Carattino <aquiles@aquicarattino.com>
2017-11-29 18:20:46 +0100

    small comments

--- pharos/config/config_example.py
+++ pharos/config/config_example.py
@@ -2,12 +2,16 @@
 
 import PyDAQmx as nidaq
 
+# Defaults for the NI card
 ni_buffer = 5000  # When acquiring in continuous mode, how big is the buffer.
-ni_measure_mode = nidaq.DAQmx_Val_Diff
+ni_measure_mode = nidaq.DAQmx_Val_Diff  #
 ni_trigger_edge = nidaq.DAQmx_Val_Rising
 ni_start_edge = nidaq.DAQmx_Val_Rising
 ni_read_timeout = 10
+
+# Defaults for GUI
 monitor_read_scan = 30  # How often (in milliseconds) do we update the signal during 1 wavelength sweep
 
-laser_update = 500  # How often (in milliseconds) the laser properties are updated.
-laser_refresh = 0.1 # How often (in seconds) to check if the laser has finished scanning
+# Defaults for the laser (mainly for sleep or timer arguments)
+laser_update = 500   #  How often (in milliseconds) the laser properties are updated.
+laser_refresh = 0.1  #  How often (in seconds) to check if the laser has finished scanning
